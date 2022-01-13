2d4d058adf67754894ae6004e65ca72ff3e3fa4e
Deep Patel <dmpatel@andrew.cmu.edu>
2022-01-13 12:29:35 -0500

    implemented function for getting all neighbors

--- day23/part1.py
+++ day23/part1.py
@@ -32,6 +32,66 @@ def getCheapestCost(start, goal):
     pass
 
 
+def makeMove(state, startIdx, endIdx, cost):
+    amphipod = state[startIdx]
+
+    if amphipod == "A":
+        cost += 1
+    if amphipod == "B":
+        cost += 10
+    if amphipod == "C":
+        cost += 100
+    if amphipod == "D":
+        cost += 1000
+
+    if startIdx < endIdx:
+        newState = state[0:startIdx] + "." + state[startIdx+1:endIdx] + amphipod + state[endIdx+1]
+    else:
+        newState = state[0:endIdx] + amphipod + state[endIdx+1:startIdx] + "." + state[startIdx+1]
+
+    return newState, cost
+
+
+def getNeighbors(state, cost):
+    neighbors = []
+
+    # move amphidpods in the top row (0-10, consider moving downards for 2, 4, 6, 8)
+    for i in range(11):
+        if state[i] != ".":
+            if 0 <= i - 1 and state[i - 1] == ".":
+                # can move to the left
+                neighbors.append(makeMove(state, i, i - 1, cost))
+            if i + 1 <= 10 and state[i + 1] == ".":
+                # can move to the right
+                neighbors.append(makeMove(state, i, i + 1, cost))
+
+            # index to move to (if possible)
+            j = 10 + i // 2
+            if (i == 2 or i == 4 or i == 6 or i == 8) and state[j] == ".":
+                # can move down
+                neighbors.append(makeMove(state, i, j, cost))
+
+    # move amphipods in the side rooms (11-18, move up or down)
+    for i in range(11, 19):
+        if state[i] != ".":
+
+            # make up moves (check if leaving room)
+            if (i == 11 or i == 12 or i == 13 or i == 14):
+                j = (i - 10) * 2
+                if state[j] == ".":
+                    # can leave side room
+                    neighbors.append(makeMove(state, i, j, cost))
+            else:
+                if state[i - 4] == ".":
+                    neighbors.append(makeMove(state, i, i - 4, cost))
+
+            # make down moves
+            if i + 4 <= 18 and state[i + 4] == ".":
+                neighbors.append(makeMove(state, i, i + 4, cost))
+
+    return neighbors
+
+
 def main():
 
     # 0-10 = top row
